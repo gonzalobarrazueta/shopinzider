3554375ce626840c8c34325c4a28bae4cd9847c5
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-08-10 16:08:31 -0500

    feat: Initialize locale data for date formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart'; // Add thi
 
-void main() {
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_ES', null); // Initiali
+
   runApp(const MyApp());
 }
 
