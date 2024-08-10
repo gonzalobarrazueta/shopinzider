82045328a3244b5efe0a41b7549b24ddc04fd78a
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2024-08-10 16:25:30 -0500

    feat: Add TransactionsPerDay widget for displaying transaction details

---
+++ lib/widgets/transactions_per_day.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactionsPerDay extends StatelessWidget {
+
+  final DateTime date;
+  final String lote;
+  final String monto;
+  final String terminal;
+
+  TransactionsPerDay({
+    required this.date,
+    required this.lote,
+    required this.monto,
+    required this.terminal
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final DateFormat dateFormat = DateFormat("d MMMM yyyy", "es_ES");
+    String formattedDate = dateFormat.format(date);
+
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9, // 90% of screen width
+      decoration: BoxDecoration(
+        color: Colors.white, // Background color for the box
+        borderRadius: BorderRadius.circular(10), // Rounded corners
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 7,
+            offset: Offset(0, 3), // Shadow position
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        child: Column(
+          children: [
+            // First Row with no columns
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  formattedDate, // Display the provided text
+                  style: const TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10), // Spacing between the rows
+            // Second Row with 2 columns
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'LOTE: $lote',
+                        style: const TextStyle(
+                          fontSize: 14
+                        ),
+                      ),
+                      Text(
+                        'TERMINAL: $terminal',
+                        style: const TextStyle(
+                          fontSize: 14
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'MONTO: S/.$monto',
+                        style: const TextStyle(
+                          fontSize: 14
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
